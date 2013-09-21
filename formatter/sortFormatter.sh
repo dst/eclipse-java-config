f3fc13242fbe160a5d979668e3e4d71c88a8a922
Dariusz Stefanski <d.a.stefanski@gmail.com>
2013-09-21 03:30:13 +0200

    Script for sorting formatter file

---
+++ formatter/sortFormatter.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dariusz Stefanski
+# 21.09.2013
+#
+# It sorts a file with Eclipse code formatter,
+# but it doesn't destroy XML schema.
+
+file="formatter.xml "
+tmpFile="/tmp/formatter_tmp.txt"
+
+head -n3 $file > $tmpFile
+grep setting $file | sort >> $tmpFile
+tail -n2 $file >> $tmpFile
+
+mv $tmpFile $file
